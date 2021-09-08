166fb7ef41bdf7e3948e8aef08ec9c8989ab1092
Emre Yavuz <yunusemreyavuz58@gmail.com>
2021-09-08 09:58:53 +0300

    Add project files.

---
+++ kisilistesi_class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kisilistesi_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kisiler ks = new kisiler();
+            string ads;
+            Console.Write("İsim Girin: ");
+            ads = Console.ReadLine();
+            ks.kisilistesi(ads);
+            Console.Read();
+        }
+    }
+}
